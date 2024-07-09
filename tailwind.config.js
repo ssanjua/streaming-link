badaf0d219bef8b83d300e20bb629cc104169e1b
ssanjua <ppaupallares@gmail.com>
2024-07-09 10:26:20 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        green: '#0a996f',
+        blue: '#0a6789',
+        red: '#cf0638',
+        orange: '#fa6632',
+        yellow: '#fecd23',
+				pink: '#E54980',
+				gray: '#1B1B1D',
+				purple: '#B43EFE',
+				dark: '#222222',
+				light: '#E1E1E1'
+      }
     },
   },
   plugins: [],
